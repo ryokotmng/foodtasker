8d071c2645540513bf2fa4119bdcaa992f0b2567
ryokotmng <ryokotmng@gmail.com>
2017-08-30 23:54:29 +0900

    settings

--- foodtaskerapp/urls.py
+++ foodtaskerapp/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from foodtaskerapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home'),
 ]
