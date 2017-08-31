b2589884290441e10262b05e216449989c0dd0ad
ryokotmng <ryokotmng@gmail.com>
2017-08-31 10:28:22 +0900

    sign in and landing page created

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -1,23 +1,16 @@
-"""foodtasker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 from foodtaskerapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^restaurant/sign-in/$', auth_views.login,
+        {'template_name': 'restaurant/sign_in.html'},
+        name = 'restaurant-sign-in'),
+    url(r'^restaurant/sign-out', auth_views.logout,
+        {'next_page': '/'},
+        name = 'restaurant-sign-out'),
+    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home'),
 ]
--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return redirect(restaurant_home)
+
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html')
