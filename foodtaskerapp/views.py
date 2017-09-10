c845f61fa09ca9837c85843c253d4a72b131eb47
ryokotmng <ryokotmng@gmail.com>
2017-09-10 19:47:24 +0900

    Sign in & sign out for restaurant owners

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return redirect(restaurant_home)
 
+@login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
     return render(request, 'restaurant/home.html')
