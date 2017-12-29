c366d673ee7fa100f29b1c356637fd70e75b5ce6
ryokotmng <ryokotmng@gmail.com>
2017-12-29 13:15:25 +0900

    added driver model

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -19,3 +19,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+class Driver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='driver')
+    avatar = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
