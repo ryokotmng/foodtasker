aca285fa52c7d9a3062529f8e24dd09f6d240fe0
ryokotmng <ryokotmng@gmail.com>
2017-12-29 13:14:57 +0900

    added customer model

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -10,3 +10,12 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
+    avatar = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
