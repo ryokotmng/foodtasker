b2c6136bde70a0ce9a8dfaa899657eeff3a9364a
ryokotmng <ryokotmng@gmail.com>
2017-12-29 13:16:27 +0900

    updated admin.py

--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Customer, Driver
 
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
