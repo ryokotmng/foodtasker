4da9a24c5a08781230a23d841c60e544c310b939
ryokotmng <ryokotmng@gmail.com>
2018-01-03 17:09:47 +0900

    created order and order details model

--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from foodtaskerapp.models import Restaurant, Customer, Driver, Meal
+from foodtaskerapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
