c1f6bd01abeef0f2aa03766fd1d0b38aed51c33b
ryokotmng <ryokotmng@gmail.com>
2018-01-05 15:54:15 +0900

    get meals api

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -12,10 +12,15 @@ def customer_get_restaurants(request):
 
     return JsonResponse({"restaurants": restaurants})
 
-def customer_get_meals(request):
-    return JsonResponse({})
+def customer_get_meals(request, restaurant_id):
+    meals = MealSerializer(
+        Meal.objects.filter(restaurant_id = restaurant_id).order_by("-id"),
+        many = True,
+        context = {"request": request}
+    ).data
+    return JsonResponse({"meals": meals})
 
-def customer_add_order(request):
+def customer_add_order(request, restaurant_id):
     return JsonResponse({})
 
 def customer_get_latest_order(request):
--- foodtaskerapp/serializers.py
+++ foodtaskerapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Meal
 
 class RestaurantSerializer(serializers.ModelSerializer):
     logo = serializers.SerializerMethodField()
@@ -13,3 +13,15 @@ class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = ("id", "name", "phone", "address", "logo")
+
+class MealSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, meal):
+        request = self.context.get('request')
+        image_url = meal.image.url
+        return request.build_absolute_uri(image_url)
+
+    class Meta:
+        model = Meal
+        fields = ("id", "name", "short_description", "image", "price")
