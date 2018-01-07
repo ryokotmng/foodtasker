d1a0298bcf8e87f870bde9a14a8df55e884a9538
ryokotmng <ryokotmng@gmail.com>
2018-01-07 15:43:01 +0900

    create order api

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -1,6 +1,12 @@
-from django.http import JsonResponse
+import json
 
-from foodtaskerapp.models import Restaurant, Meal
+from django.utils import timezone
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from oauth2_provider.models import AccessToken
+
+from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetails
 from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer
 
 def customer_get_restaurants(request):
@@ -20,8 +26,52 @@ def customer_get_meals(request, restaurant_id):
     ).data
     return JsonResponse({"meals": meals})
 
+@csrf_exempt
 def customer_add_order(request, restaurant_id):
-    return JsonResponse({})
+
+    if request.method == "POST":
+        # Get token
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+            expires__gt = timezone.now())
+
+        # Get profile
+        customer = access_token.user.customer
+
+        # Check whether customer has any order that is not delivered
+        if Order.objects.filter(customer = customer).exclude(status = Order.DELIVERED):
+            return JsonResponse({"status": "failed", "error": "Your last order must be completed."})
+
+        # Check Address
+        if not request.POST["address"]:
+            return JsonResponse({"status": "failed", "error": "Address is required."})
+
+        # Get Order Details
+        order_details = json.loads(request.POST["order_details"])
+
+        order_total = 0
+        for meal in order_details:
+            order_total += Meal.objects.get(id = meal["meal_id"]).price * meal["quantity"]
+
+        if len(order_details) > 0:
+            # Step 1 - Create an Order
+            order = Order.objects.create(
+                customer = customer,
+                restaurant_id = request.POST["restaurant_id"],
+                total = order_total,
+                status = Order.COOKING,
+                address = request.POST["address"]
+            )
+
+            # Step 2 - Create Order details
+            for meal in order_details:
+                OrderDetails.objects.create(
+                    order = order,
+                    meal_id = meal["meal_id"],
+                    quantity = meal["quantity"],
+                    sub_total = Meal.objects.get(id = meal["meal_id"]).price * meal["quantity"]
+                )
+
+            return JsonResponse({"status": "success"})
 
 def customer_get_latest_order(request):
     return JsonResponse({})
